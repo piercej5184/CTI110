042367f3e9f776145cea7aad80b51bcba3deea77
piercej5184 <31550717+piercej5184@users.noreply.github.com>
2017-09-01 15:45:43 -0400

    Add files via upload

---
+++ M2T1_Pierce.py
@@ -0,0 +1,41 @@
+# CTI 110
+# M2T1 - Sales Prediction
+# 8/30
+# Jamire Pierce
+
+
+# Suggested variables: totalSales, annualProfit
+
+
+# ask the user for total sales
+totalSales = int(input("Type total sales: "))
+
+# calculate the profit amount
+annualProfit = 0.23 * totalSales
+
+
+# print the profit amount
+print(annualProfit)
+
+
+
+
+
+
+
+
+'''
+# program to add two numbers
+#userTyped = input("Type a number: ")
+#userNumber = int(userTyped)
+
+# get the numbers
+firstNumber = int(input("Type a number: "))
+secondNumber = int(input("Type another number: "))
+
+# add them
+answer = firstNumber + secondNumber
+
+# print result
+print("The answer is", answer)
+'''
